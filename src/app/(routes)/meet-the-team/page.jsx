3899cac52e3f370ae1f27024ba9b607a560d1219
Bruno Grecchi <b_grecchi@hotmail.com>
2024-05-13 23:07:37 +0100

    first commit: folder structure

---
+++ src/app/(routes)/meet-the-team/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MeetTheTeam() {
+  return (
+    <>
+      <h1>Meet The Team</h1>
+    </>
+  );
+}
---
+++ src/app/(routes)/work-with-us/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function WorkWithUs() {
+  return <h1>Work With us</h1>;
+  <p>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam autem hic
+    unde, eos totam vitae dolorum. Debitis sapiente cupiditate nostrum ullam,
+    rem sint officiis recusandae fugit tempore dolores corporis, earum labore
+    libero totam. Reprehenderit, vero molestias quas itaque delectus deserunt in
+    incidunt saepe deleniti commodi, harum optio similique et vel fugit
+    dignissimos dolor aut. Vitae hic quo necessitatibus.
+  </p>;
+}
