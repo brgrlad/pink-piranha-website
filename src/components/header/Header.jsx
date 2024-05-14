0663ccc4edcef806d4942d81e91dd4c73871e712
Bruno Grecchi <b_grecchi@hotmail.com>
2024-05-14 11:14:29 +0100

    ADDED: placeholder img for logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import logo from "../../../public/gradients/logo.png";
+import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full px-4 py-2 ">
-      <h2 className="text-pink-400">Pink Piranha</h2>
+    <header className="flex justify-between items-start w-full px-5 py-5">
+      <Image src={logo} width={100} height={100} alt="Pink Piranha logo" />
+
       <button
         type="button"
-        class=" text-pink-800 bg-pink-400 border-transparent mt-[-50px] focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-5 py-1 text-center mb-2 h-10 flex items-end"
+        class=" text-pink-800 bg-pink-300 border-transparent mt-[-35px] focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-8 py-1 text-center mb-2 h-10 flex items-end"
       >
         STAFF NEEDED: COME WORK WITH US!
       </button>
       <button
         type="button"
-        class=" bg-transparent border-4 border-pink-400 text-pink-400 focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-5 py-2.5 text-center mb-2"
+        class=" bg-transparent border-4 border-pink-400 text-pink-400 focus:outline-none focus:ring-4  font-medium rounded-full text-sm px-8 py-2 text-center mb-2"
       >
         MENU
       </button>
