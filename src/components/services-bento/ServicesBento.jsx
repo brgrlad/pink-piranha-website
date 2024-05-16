e3f7b1e2b4d0c3fdebb1c0323af5876d21c853e9
Bruno Grecchi <b_grecchi@hotmail.com>
2024-05-16 12:29:52 +0100

    CHANGED: bento box grid layout (still tweaking)

--- src/components/services-bento/ServicesBento.jsx
+++ src/components/services-bento/ServicesBento.jsx
@@ -1,31 +1,61 @@
+import bentoImage from "../../../public/crowd.jpeg";
+import Image from "next/image";
+
 export default function ServicesBento() {
   return (
-    <section className="w-full py-10 md:px-10 px-5 flex">
+    <section className="w-full py-10 md:px-10 px-5 flex flex-end mt-[-80px]">
       {/* left side */}
 
-      <div class="grid grid-cols-1 gap-y-4 w-1/3">
-        <div class="flex items-center justify-center h-48 bg-blue-500 text-white">
-          1
+      <div class="grid grid-cols-1 gap-y-[10px] w-1/3 overflow-hidden justify-end items-baseline border-2">
+        <div class="flex h-[400px] w-[400px] self-end">
+          <Image
+            src={bentoImage}
+            alt="placeholder image"
+            class="rounded-3xl h-full"
+          />
         </div>
-        <div class="flex items-center justify-center h-48 bg-blue-500 text-white">
-          2
+
+        <div class="flex h-[400px] w-[400px] self-end">
+          <Image
+            src={bentoImage}
+            alt="placeholder image"
+            class="rounded-3xl h-full"
+          />
         </div>
-        <div class="flex items-center justify-center h-48 bg-blue-500 text-white">
-          3
+        <div class="flex h-[400px] w-[400px] self-end">
+          <Image
+            src={bentoImage}
+            alt="placeholder image"
+            class="rounded-3xl h-full"
+          />
         </div>
       </div>
 
       {/* right side */}
 
-      <div class="grid grid-cols-2 gap-4 w-2/3 px-4">
-        <div class="flex items-center justify-center h-72 col-span-2 bg-blue-500 text-white self-end mt-[115px] ">
-          4
+      <div class="grid grid-cols-2 gap-[10px] w-2/3 px-4 items-end overflow-hidden">
+        <div class="flex items-center justify-center h-[200px] col-span-1 self-end"></div>
+        <div class="flex items-center justify-center h-[200px] col-span-1 self-end"></div>
+        <div class="flex items-center justify-center h-[600px] col-span-2 self-end">
+          <Image
+            src={bentoImage}
+            alt="placeholder image"
+            class="rounded-3xl h-full"
+          />
         </div>
-        <div class="flex items-center justify-center h-48 bg-blue-500 text-white self-end">
-          5
+        <div class="flex items-center justify-center h-[400px] col-span-1 self-end">
+          <Image
+            src={bentoImage}
+            alt="placeholder image"
+            class="rounded-3xl h-full"
+          />
         </div>
-        <div class="flex items-center justify-center h-48 bg-blue-500 text-white self-end">
-          6
+        <div class="flex items-center justify-center h-[400px] col-span-1 self-end">
+          <Image
+            src={bentoImage}
+            alt="placeholder image"
+            class="rounded-3xl h-full"
+          />
         </div>
       </div>
     </section>
