54f3852850e0d437270eded0ebbc0f26d20d9717
Bruno Grecchi <b_grecchi@hotmail.com>
2024-05-22 10:51:44 +0200

    ADDED: media queries for what we do section

--- src/components/what-we-do/WhatWeDo.jsx
+++ src/components/what-we-do/WhatWeDo.jsx
@@ -5,12 +5,12 @@ import logo from "../../../public/gradients/logo.png";
 
 export default function WhatWeDo() {
   return (
-    <section class="bg-pink-700 text-pink-200 p-10">
+    <section class="bg-pink-700 text-pink-200 sm:p-10 p-1 ">
       <h2 class="text-4xl lg:text-nowrap md:text-4xl lg:text-6xl xl:text-7xl mb-20">
         WHAT WE DO
       </h2>
 
-      <div class="grid grid-cols-2 gap-y-1 gap-x-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-y-2 gap-x-2 sm-gap-x-0">
         <LinkCard
           title={"Event Staffing"}
           content={
@@ -23,34 +23,29 @@ export default function WhatWeDo() {
         <LinkCard
           title={"Event Staffing"}
           content={
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aliquam earum? Officiis ducimus ipsum laboriosam."
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aliquam earum? Officiis ducimus ipsum laborios."
           }
-          height={"250px"}
+          height="250px"
           width="full"
-        />
-
-        {/* <Image
-          class="absolute top-1/2 left-1/2 "
-          src={logo}
-          width={100}
-          height={100}
-          alt="Pink Piranha logo"
-        /> */}
-        <LinkCard
-          title={"Event Staffing"}
-          content={
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aliquam earum? Officiis ducimus ipsum laboriosam."
-          }
-          height={"250px"}
-          width="full"
+          marginBottom="0"
         />
         <LinkCard
           title={"Event Staffing"}
           content={
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aliquam earum? Officiis ducimus ipsum laboriosam."
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aliquam earum? Officiis ducimus ipsum laborios."
           }
-          height={"250px"}
+          height="250px"
           width="full"
+          marginBottom="0"
+        />
+        <LinkCard
+          title={"Event Staffing"}
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, aliquam earum? Officiis ducimus ipsum laborios."
+          }
+          height="250px"
+          width="full"
+          marginBottom="0"
         />
       </div>
     </section>
