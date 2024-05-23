b447ce339a3d17228f6524e2e0c6bc40a77dd132
Bruno Grecchi <b_grecchi@hotmail.com>
2024-05-23 23:43:22 +0200

    UPDATED: hero section text/media queries

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,10 +8,8 @@ export default function Hero() {
     <section className="hero bg-pink-900 text-pink-200 md:px-10 px-5 py-10 flex">
       <Header />
 
-      {/* add card bellow to its own component? in case it needs to be reused  */}
-
       <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
-        <div class="mr-auto place-self-center lg:col-span-7">
+        <div class="mr-auto place-self-center col-span-12 xl:col-span-7">
           <h2 class="max-w-2xl mb-4 mt-20 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl border-0">
             We specialize in the art of assembling exceptional teams that bring
             your vision to life.
