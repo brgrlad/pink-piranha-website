62471227d7a131b00ddc52c9ddec401b859ccf61
Bruno Grecchi <b_grecchi@hotmail.com>
2024-05-29 13:47:10 +0100

    ADDED: Icons and list to services card

--- src/components/services-card/ServicesCard.jsx
+++ src/components/services-card/ServicesCard.jsx
@@ -3,47 +3,27 @@ import Link from "next/link";
 export default function ServicesCard({
   title,
   content,
-  link,
-  linkDescription = "Know more",
+  icon,
   width = "100%",
-  height = "200px",
+  height = "400px",
   marginBottom = "20px",
 }) {
   return (
     <div
       style={{ width, height, marginBottom }}
-      className={`py-1 md:py-2 px-4 bg-pink-900 rounded-xl text-pink-200 flex flex-col justify-center`}
+      className={`py-1 md:py-2 px-4 bg-pink-900 rounded-xl text-pink-200 flex flex-col justify-start`}
     >
-      <h3 className="mb-1 md:text-3xl text-lg">{title}</h3>
-      <ul>
+      {icon}
+
+      {/* <h3 className="mb-1 text-[22px]">{title}</h3> */}
+      <p className="mb-5 text-[18px] font-bold">{title}</p>
+      <ul className="text-sm list-disc ml-2">
         {content.map((service) => (
           <li key={service}>
-            <p className="mb-1 text-sm ">{service}</p>{" "}
+            <p className="mb-5">{service}</p>
           </li>
         ))}
       </ul>
-
-      {link && (
-        <Link
-          href={`${link}`}
-          className="flex justify-end items-center w-full h-3 "
-        >
-          {linkDescription}
-          <svg
-            className="h-8 w-8 ml-2"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M17 8l4 4m0 0l-4 4m4-4H3"
-            />
-          </svg>
-        </Link>
-      )}
     </div>
   );
 }
--- src/components/what-we-do/WhatWeDo.jsx
+++ src/components/what-we-do/WhatWeDo.jsx
@@ -28,6 +28,22 @@ export default function WhatWeDo() {
             "Box Office",
             "Stewards",
           ]}
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-[50px] my-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z"
+              />
+            </svg>
+          }
         />
         <ServicesCard
           title={"ENTERTAINMENT"}
@@ -38,6 +54,22 @@ export default function WhatWeDo() {
             "Fire Breathers",
             "Face Painting",
           ]}
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-[50px] my-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m9 9 10.5-3m0 6.553v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 1 1-.99-3.467l2.31-.66a2.25 2.25 0 0 0 1.632-2.163Zm0 0V2.25L9 5.25v10.303m0 0v3.75a2.25 2.25 0 0 1-1.632 2.163l-1.32.377a1.803 1.803 0 0 1-.99-3.467l2.31-.66A2.25 2.25 0 0 0 9 15.553Z"
+              />
+            </svg>
+          }
         />
         <ServicesCard
           title={"EVENT MANAGEMENT"}
@@ -48,6 +80,22 @@ export default function WhatWeDo() {
             "Brand Ambassadors",
             "Dressing Room Management",
           ]}
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-[50px] my-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+              />
+            </svg>
+          }
         />
         <ServicesCard
           title={"OPERATIONAL STAFF"}
@@ -57,6 +105,22 @@ export default function WhatWeDo() {
             "Bar Management",
             "Ticketing",
           ]}
+          icon={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="size-[50px] my-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+              />
+            </svg>
+          }
         />
       </div>
     </section>
