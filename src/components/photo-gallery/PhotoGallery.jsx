18a81f6b9e55de16bec48714e2ddac2ff93302b3
Bruno Grecchi <b_grecchi@hotmail.com>
2024-06-11 21:58:56 +0100

    FIXED: bubbles image and wavy section divider

--- src/components/photo-gallery/PhotoGallery.jsx
+++ src/components/photo-gallery/PhotoGallery.jsx
@@ -1,31 +1,24 @@
 import Image from "next/image";
 
-// import staff01 from "/img/photos/staff01.jpg";
-// import staff02 from "/img/photos/staff02.jpg";
-// import staff03 from "/img/photos/staff03.jpg";
-// import staff04 from "/img/photos/staff04.jpg";
-// import staff05 from "/img/photos/staff05.jpg";
-// import waves from "/img/waves/svg-2.png";
-// import bubbleTrio from "/img/bubbles/bubble_trio_1.png";
-
 export default function PhotoGallery() {
   return (
     <section className="relative rounded-4xl shadow-lg" id="photo-gallery">
       <Image
         src="/img/waves/svg-2.png"
         alt="section divider: an illustration of sea waves in shades of pink"
-        className="section-divider h-[150px]"
+        className="section-divider min-w-full h-[150px]"
         width={500}
-        height={500}
+        height={100}
       />
 
-      {/* <Image
-        src="/img/bubbles/bubble_trio1.png"
+      <Image
+        src="/img/bubbles/bubble_trio_1.png"
         alt="bubles"
         className="absolute w-[150px] md:w-[200px] left-[60%]  lg:w-[250px]  md:left-[30%] lg:left-[45%] xl:left-[40%] top-[-6%] rotate-[150deg] z-40"
         width={500}
-        height={500}
-      /> */}
+        height={150}
+      />
+
       <div className="contentWrapper  md:px-10 px-5 -mt-[20px] rounded-xl py-10 bg-medium-pink bg-gradient-to-b from-medium-pink via-medium-pink via-60% to-dark-pink">
         <h2 className="text-4xl lg:text-nowrap md:text-4xl lg:text-6xl xl:text-7xl text-dark-pink">
           PHOTO GALLERY
@@ -65,7 +58,7 @@ export default function PhotoGallery() {
             className="aspect-square col-span-1 object-cover rounded-xl"
           />
           <Image
-            width={500}
+            width={1000}
             height={500}
             src="/img/photos/staff05.jpg"
             alt="placeholder"
