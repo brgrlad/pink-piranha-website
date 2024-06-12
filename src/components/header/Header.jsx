7064b5cc25d0d6026860ca1ac534cc671e1d80bd
Bruno Grecchi <b_grecchi@hotmail.com>
2024-06-12 22:20:37 +0100

    updated: menu button positioning and hiring button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   let [showMenu, setShowMenu] = useState(false);
 
   return (
-    <header className="fixed overflow-x-hidden left-0 top-0 flex justify-between items-start w-full p-5 z-50 ">
+    <header className="fixed overflow-x-hidden left-0 top-0 flex justify-between xm:self-center sm:items-start w-full p-5 z-50 ">
       <Link href="/#home">
         <Image src={logo} width={100} height={100} alt="Pink Piranha logo" />
       </Link>
@@ -18,7 +18,7 @@ export default function Header() {
       <Link href="work-with-us">
         <button
           type="button"
-          className="top-5 left-28 text-dark-pink  bg-light-pink border border-dark-pink mt-[-35px] focus:outline-none focus:ring-4 text-[8px] md:text-sm md:font-medium rounded-full text-sm px-4 py-1 text-center mb-2 h-10 flex items-end"
+          className=" text-dark-pink  bg-light-pink border border-dark-pink   mt-[-40px]  sm:mt-[-35px] focus:outline-none focus:ring-4 text-[10px] sm:text-sm md:font-medium rounded-full text-sm px-2 md:px-4 py-0 text-center mb-2 h-10 flex items-end"
         >
           WE`RE HIRING!
         </button>
@@ -26,7 +26,7 @@ export default function Header() {
 
       <button
         type="button"
-        className="shadow-3xl border border-dark-pink text-dark-pink bg-light-pink font-bold rounded-full text-sm  px-8 py-2 text-center mb-2 z-50"
+        className="shadow-3xl border border-dark-pink text-dark-pink bg-light-pink font-bold rounded-full text-sm h-[40px] self-center px-8 py-2 text-center mb-2 z-50"
         onClick={() => setShowMenu((prev) => !showMenu)}
       >
         {showMenu ? "CLOSE" : "MENU"}
