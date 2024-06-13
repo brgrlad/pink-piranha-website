b2b7187c90d795e327869f6594ea8b6afb71a202
Bruno Grecchi <b_grecchi@hotmail.com>
2024-06-13 22:23:47 +0100

    UPDATED: drop down media queries

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,24 +5,17 @@ import logo from "../../../public/img/pink-piranha-logo.png";
 import LinkCard from "../link-card/LinkCard";
 import { useEffect } from "react";
 
-// -- setShowMenu IS BEING PASSED AS A PROP FOR EACH OF THE MENU LINKS.
-// -- setShowMenu WILL OPEN/CLOSE THE DROPDOWN MENU
 export default function Menu({ showMenu, setShowMenu }) {
-  useEffect(() => {
-    console.log("test");
-    console.log(showMenu);
-  }, [showMenu]);
   return (
-    // <nav className="menuBg fixed z-40 top-0 right-0 bg-pink-400 m-2 rounded-xl p-1 lg:p-5 w-[97%] md:w-[60%] xl:w-[50%] selection:bg-pink-300 shadow-xl bg-gradient-to-r from-pink-400 via-medium-pink to-dark-pink h-auto ">
-
     <nav
-      className={`menuBg fixed z-40 top-0 right-0 bg-pink-400 m-2 rounded-xl p-1 lg:p-5 w-[97%] md:w-[60%] xl:w-[50%] selection:bg-pink-300 shadow-xl bg-gradient-to-r from-pink-400 via-medium-pink to-dark-pink h-auto ${
+      className={`menuBg fixed z-40 top-1 right-0 bg-pink-400 m-2 rounded-xl p-1 lg:p-5 w-[97%] md:w-[60%] xl:w-[50%] selection:bg-pink-300 shadow-xl bg-gradient-to-r from-pink-400 via-medium-pink to-dark-pink h-dvh sm:h-auto ${
         showMenu ? "fade-in" : ""
       }`}
     >
       <Image src={logo} width={100} height={100} alt="Pink Piranha logo" />
+
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 px-4 pt-4 pb-2">
-        <div className="left-column md:order-1 order-2">
+        <div className="left-column sm:order-1 order-2">
           <LinkCard
             onClick={() => setShowMenu(false)}
             title={"REACH OUT"}
@@ -41,7 +34,7 @@ export default function Menu({ showMenu, setShowMenu }) {
             link="/#contact-us"
           />
         </div>
-        <div className="right-column md:order-2 order-1">
+        <div className="right-column sm:order-2 order-1">
           <ul className="h-full pb-1 text-right flex flex-col justify-between text-2xl text-light-pink font-bold">
             <li className="py-2">
               <Link
